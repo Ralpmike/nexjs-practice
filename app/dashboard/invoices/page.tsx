468acce7da1b300ebdcf34ac776d6d8c8b5ef4de
Ralpmike <ralphmike996@gmail.com>
2025-10-04 12:52:35 +0100

    streaming with react suspense

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,12 @@
-export default function page() {
+export default async function page() {
+  const data = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("REsolved");
+      
+    }, 4000)
+  })
+  const result = await data
+  console.log(result)
+
   return <p>Invoices Page</p>;
 }
